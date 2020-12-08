ddf534f771b99a3eee0a58e2e3541040329058cb
Simon Ljungberg <hi@iamsim.me>
2020-12-08 23:19:16 +0100

    Move constraint builders out of the Constraint namespace for a cleaner API.
    Turns out type inference didn't work very well.

--- Sources/Tablecloth/Tablecloth.swift
+++ Sources/Tablecloth/Tablecloth.swift
@@ -74,200 +74,196 @@ public struct Constraint {
     copy.priority = priority
     return copy
   }
+}
 
-  // MARK: Creating Constraints
+// MARK: Creating Constraints
 
-  /// Generates an "equal" `Constraint` between two view's layout anchors.
-  ///
-  /// This code will constraint `subview` to be equal to the width of `view` - 16 points.
-  ///
-  ///     view.addSubview(subview) {
-  ///       equal(\.widthAnchor, constant: -16)
-  ///     }
-  ///
-  /// Or you can constrain the view to another view in the view hierarchy. This for example will make `subview`'s width
-  /// the same as `imageView`s height.
-  ///
-  ///     view.addSubview(subview) {
-  ///       equal(\.widthAnchor, \.heightAnchor, of: imageView)
-  ///     }
-  ///
-  /// - Parameter from: A key path to the anchor to equate to on the other view.
-  /// - Parameter to: An optional keypath to the anchor to equate `keyPath` to. If `nil` `keyPath` will be used as the
-  ///     target as well.
-  /// - Parameter otherView: The view to constrain the target to. Defaults to the view's parent.
-  ///
-  /// - Returns: A `Constraint` that creates an equality constraint.
-  public static func equal<Anchor>(
-    _ from: KeyPath<UIView, Anchor>,
-    _ to: KeyPath<UIView, Anchor>? = nil,
-    of otherView: UIView? = nil
-  ) -> Constraint where Anchor: NSLayoutDimension {
-    .init { view, parent, constant, multiplier in
-      let anchor = view[keyPath: from]
-      let comparison = (otherView ?? parent)[keyPath: to ?? from]
-      return anchor.constraint(equalTo: comparison, multiplier: multiplier, constant: constant)
-    }
+/// Generates an "equal" `Constraint` between two view's layout anchors.
+///
+/// This code will constraint `subview` to be equal to the width of `view` - 16 points.
+///
+///     view.addSubview(subview) {
+///       equal(\.widthAnchor, constant: -16)
+///     }
+///
+/// Or you can constrain the view to another view in the view hierarchy. This for example will make `subview`'s width
+/// the same as `imageView`s height.
+///
+///     view.addSubview(subview) {
+///       equal(\.widthAnchor, \.heightAnchor, of: imageView)
+///     }
+///
+/// - Parameter from: A key path to the anchor to equate to on the other view.
+/// - Parameter to: An optional keypath to the anchor to equate `keyPath` to. If `nil` `keyPath` will be used as the
+///     target as well.
+/// - Parameter otherView: The view to constrain the target to. Defaults to the view's parent.
+///
+/// - Returns: A `Constraint` that creates an equality constraint.
+public func equal<Anchor>(
+  _ from: KeyPath<UIView, Anchor>,
+  _ to: KeyPath<UIView, Anchor>? = nil,
+  of otherView: UIView? = nil
+) -> Constraint where Anchor: NSLayoutDimension {
+  .init { view, parent, constant, multiplier in
+    let anchor = view[keyPath: from]
+    let comparison = (otherView ?? parent)[keyPath: to ?? from]
+    return anchor.constraint(equalTo: comparison, multiplier: multiplier, constant: constant)
   }
+}
 
-  /// Generates an "equal" `Constraint` between two view's layout anchors.
-  ///
-  /// This code will constraint `subview` to be equal to the width of `view` - 16 points.
-  ///
-  ///     view.addSubview(subview) {
-  ///       equal(\.widthAnchor).constant(-16)
-  ///     }
-  ///
-  /// Or you can constrain the view to another view in the view hierarchy. This for example will make `subview`'s width
-  /// the same as `imageView`s height.
-  ///
-  ///     view.addSubview(subview) {
-  ///       equal(\.widthAnchor, \.heightAnchor, of: imageView)
-  ///     }
-  ///
-  /// - Parameter from: A key path to the anchor to equate to on the other view.
-  /// - Parameter to: An optional keypath to the anchor to equate `keyPath` to. If `nil` `keyPath` will be used as the
-  ///     target as well.
-  /// - Parameter otherView: The view to constrain the target to. Defaults to the view's parent.
-  ///
-  /// - Returns: A `Constraint` that creates an equality constraint.
-  public static func equal<Axis, Anchor>(
-    _ from: KeyPath<UIView, Anchor>,
-    _ to: KeyPath<UIView, Anchor>? = nil,
-    of otherView: UIView? = nil
-  ) -> Constraint where Anchor: NSLayoutAnchor<Axis> {
-    .init { view, parent, constant, _ in
-      let anchor = view[keyPath: from]
-      let comparison = (otherView ?? parent)[keyPath: to ?? from]
-      return anchor.constraint(equalTo: comparison, constant: constant)
-    }
+/// Generates an "equal" `Constraint` between two view's layout anchors.
+///
+/// This code will constraint `subview` to be equal to the width of `view` - 16 points.
+///
+///     view.addSubview(subview) {
+///       equal(\.widthAnchor).constant(-16)
+///     }
+///
+/// Or you can constrain the view to another view in the view hierarchy. This for example will make `subview`'s width
+/// the same as `imageView`s height.
+///
+///     view.addSubview(subview) {
+///       equal(\.widthAnchor, \.heightAnchor, of: imageView)
+///     }
+///
+/// - Parameter from: A key path to the anchor to equate to on the other view.
+/// - Parameter to: An optional keypath to the anchor to equate `keyPath` to. If `nil` `keyPath` will be used as the
+///     target as well.
+/// - Parameter otherView: The view to constrain the target to. Defaults to the view's parent.
+///
+/// - Returns: A `Constraint` that creates an equality constraint.
+public func equal<Axis, Anchor>(
+  _ from: KeyPath<UIView, Anchor>,
+  _ to: KeyPath<UIView, Anchor>? = nil,
+  of otherView: UIView? = nil
+) -> Constraint where Anchor: NSLayoutAnchor<Axis> {
+  .init { view, parent, constant, _ in
+    let anchor = view[keyPath: from]
+    let comparison = (otherView ?? parent)[keyPath: to ?? from]
+    return anchor.constraint(equalTo: comparison, constant: constant)
   }
+}
 
-  /// Generates a "greater than or equal" `Constraint` between two view's layout anchors.
-  ///
-  /// - Parameter from: A key path to the anchor to compare to on the other view.
-  /// - Parameter to: An optional keypath to the anchor to equate `keyPath` to. If `nil` `keyPath` will be used as the
-  ///     target as well.
-  /// - Parameter otherView: The view to constrain the target to. Defaults to the view's parent.
-  ///
-  /// - Returns: A `Constraint` that creates a greater than or equal constraint.
-  public static func greaterThanOrEqual<Anchor>(
-    _ from: KeyPath<UIView, Anchor>,
-    _ to: KeyPath<UIView, Anchor>? = nil,
-    of otherView: UIView? = nil
-  ) -> Constraint where Anchor: NSLayoutDimension {
-    .init { view, parent, constant, multiplier in
-      let anchor = view[keyPath: from]
-      let comparison = (otherView ?? parent)[keyPath: to ?? from]
-      return anchor.constraint(greaterThanOrEqualTo: comparison, multiplier: multiplier, constant: constant)
-    }
+/// Generates a "greater than or equal" `Constraint` between two view's layout anchors.
+///
+/// - Parameter from: A key path to the anchor to compare to on the other view.
+/// - Parameter to: An optional keypath to the anchor to equate `keyPath` to. If `nil` `keyPath` will be used as the
+///     target as well.
+/// - Parameter otherView: The view to constrain the target to. Defaults to the view's parent.
+///
+/// - Returns: A `Constraint` that creates a greater than or equal constraint.
+public func greaterThanOrEqual<Anchor>(
+  _ from: KeyPath<UIView, Anchor>,
+  _ to: KeyPath<UIView, Anchor>? = nil,
+  of otherView: UIView? = nil
+) -> Constraint where Anchor: NSLayoutDimension {
+  .init { view, parent, constant, multiplier in
+    let anchor = view[keyPath: from]
+    let comparison = (otherView ?? parent)[keyPath: to ?? from]
+    return anchor.constraint(greaterThanOrEqualTo: comparison, multiplier: multiplier, constant: constant)
   }
+}
 
-  /// Generates a "greater than or equal" `Constraint` between two view's layout anchors.
-  ///
-  /// - Parameter from: A key path to the anchor to compare to on the other view.
-  /// - Parameter to: An optional keypath to the anchor to equate `keyPath` to. If `nil` `keyPath` will be used as the
-  ///     target as well.
-  /// - Parameter otherView: The view to constrain the target to. Defaults to the view's parent.
-  ///
-  /// - Returns: A `Constraint` that creates a greater than or equal constraint.
-  public static func greaterThanOrEqual<Axis, Anchor>(
-    _ from: KeyPath<UIView, Anchor>,
-    _ to: KeyPath<UIView, Anchor>? = nil,
-    of otherView: UIView? = nil
-  ) -> Constraint where Anchor: NSLayoutAnchor<Axis> {
-    .init { view, parent, constant, _ in
-      let anchor = view[keyPath: from]
-      let comparison = (otherView ?? parent)[keyPath: to ?? from]
-      return anchor.constraint(greaterThanOrEqualTo: comparison, constant: constant)
-    }
+/// Generates a "greater than or equal" `Constraint` between two view's layout anchors.
+///
+/// - Parameter from: A key path to the anchor to compare to on the other view.
+/// - Parameter to: An optional keypath to the anchor to equate `keyPath` to. If `nil` `keyPath` will be used as the
+///     target as well.
+/// - Parameter otherView: The view to constrain the target to. Defaults to the view's parent.
+///
+/// - Returns: A `Constraint` that creates a greater than or equal constraint.
+public func greaterThanOrEqual<Axis, Anchor>(
+  _ from: KeyPath<UIView, Anchor>,
+  _ to: KeyPath<UIView, Anchor>? = nil,
+  of otherView: UIView? = nil
+) -> Constraint where Anchor: NSLayoutAnchor<Axis> {
+  .init { view, parent, constant, _ in
+    let anchor = view[keyPath: from]
+    let comparison = (otherView ?? parent)[keyPath: to ?? from]
+    return anchor.constraint(greaterThanOrEqualTo: comparison, constant: constant)
   }
+}
 
-  /// Generates a "less than or equal" `Constraint` between two view's layout anchors.
-  ///
-  /// - Parameter from: A key path to the anchor to compare to on the other view.
-  /// - Parameter to: An optional keypath to the anchor to equate `keyPath` to. If `nil` `keyPath` will be used as the
-  ///     target as well.
-  /// - Parameter otherView: The view to constrain the target to. Defaults to the view's parent.
-  ///
-  /// - Returns: A `Constraint` that creates a less than or equal constraint.
-  public static func lessThanOrEqual<Anchor>(
-    _ from: KeyPath<UIView, Anchor>,
-    _ to: KeyPath<UIView, Anchor>? = nil,
-    of otherView: UIView? = nil
-  ) -> Constraint where Anchor: NSLayoutDimension {
-    .init { view, parent, constant, multiplier in
-      let anchor = view[keyPath: from]
-      let comparison = (otherView ?? parent)[keyPath: to ?? from]
-      return anchor.constraint(lessThanOrEqualTo: comparison, multiplier: multiplier, constant: constant)
-    }
+/// Generates a "less than or equal" `Constraint` between two view's layout anchors.
+///
+/// - Parameter from: A key path to the anchor to compare to on the other view.
+/// - Parameter to: An optional keypath to the anchor to equate `keyPath` to. If `nil` `keyPath` will be used as the
+///     target as well.
+/// - Parameter otherView: The view to constrain the target to. Defaults to the view's parent.
+///
+/// - Returns: A `Constraint` that creates a less than or equal constraint.
+public func lessThanOrEqual<Anchor>(
+  _ from: KeyPath<UIView, Anchor>,
+  _ to: KeyPath<UIView, Anchor>? = nil,
+  of otherView: UIView? = nil
+) -> Constraint where Anchor: NSLayoutDimension {
+  .init { view, parent, constant, multiplier in
+    let anchor = view[keyPath: from]
+    let comparison = (otherView ?? parent)[keyPath: to ?? from]
+    return anchor.constraint(lessThanOrEqualTo: comparison, multiplier: multiplier, constant: constant)
   }
+}
 
-  /// Generates a "less than or equal" `Constraint` between two view's layout anchors.
-  ///
-  /// - Parameter from: A key path to the anchor to compare to on the other view.
-  /// - Parameter to: An optional keypath to the anchor to equate `keyPath` to. If `nil` `keyPath` will be used as the
-  ///     target as well.
-  /// - Parameter otherView: The view to constrain the target to. Defaults to the view's parent.
-  ///
-  /// - Returns: A `Constraint` that creates a less than or equal constraint.
-  public static func lessThanOrEqual<Axis, Anchor>(
-    _ from: KeyPath<UIView, Anchor>,
-    _ to: KeyPath<UIView, Anchor>? = nil,
-    of otherView: UIView? = nil
-  ) -> Constraint where Anchor: NSLayoutAnchor<Axis> {
-    .init { view, parent, constant, _ in
-      let anchor = view[keyPath: from]
-      let comparison = (otherView ?? parent)[keyPath: to ?? from]
-      return anchor.constraint(lessThanOrEqualTo: comparison, constant: constant)
-    }
+/// Generates a "less than or equal" `Constraint` between two view's layout anchors.
+///
+/// - Parameter from: A key path to the anchor to compare to on the other view.
+/// - Parameter to: An optional keypath to the anchor to equate `keyPath` to. If `nil` `keyPath` will be used as the
+///     target as well.
+/// - Parameter otherView: The view to constrain the target to. Defaults to the view's parent.
+///
+/// - Returns: A `Constraint` that creates a less than or equal constraint.
+public func lessThanOrEqual<Axis, Anchor>(
+  _ from: KeyPath<UIView, Anchor>,
+  _ to: KeyPath<UIView, Anchor>? = nil,
+  of otherView: UIView? = nil
+) -> Constraint where Anchor: NSLayoutAnchor<Axis> {
+  .init { view, parent, constant, _ in
+    let anchor = view[keyPath: from]
+    let comparison = (otherView ?? parent)[keyPath: to ?? from]
+    return anchor.constraint(lessThanOrEqualTo: comparison, constant: constant)
   }
+}
 
-  /// Creates a constant `Constraint`.
-  ///
-  /// - Parameter keyPath: The keypath to constrain to a constant size.
-  /// - Parameter constant: The constant value.
-  ///
-  /// - Returns: A constant `Constraint`.
-  public static func constant<Anchor>(
-    _ keyPath: KeyPath<UIView, Anchor>,
-    value constant: CGFloat
-  ) -> Constraint where Anchor: NSLayoutDimension {
-    .init { view, _, _constant, _ in
-      guard _constant == 0 else {
-        fatalError("""
+/// Creates a constant `Constraint`.
+///
+/// - Parameter keyPath: The keypath to constrain to a constant size.
+/// - Parameter constant: The constant value.
+///
+/// - Returns: A constant `Constraint`.
+public func constant<Anchor>(
+  _ keyPath: KeyPath<UIView, Anchor>,
+  value constant: CGFloat
+) -> Constraint where Anchor: NSLayoutDimension {
+  .init { view, _, _constant, _ in
+    guard _constant == 0 else {
+      fatalError("""
           Constant constraint was created with a `.constant()` modifier, which has no effect on this constraint. This is
           considered a programmer error.
         """)
-      }
-      return view[keyPath: keyPath].constraint(equalToConstant: constant)
     }
+    return view[keyPath: keyPath].constraint(equalToConstant: constant)
   }
-
 }
+
 
 // MARK: - Scroll View Affordances
 
-extension Constraint {
-
-  /// Generates an "equal" `Constraint` between a view and a scroll view's layout guides.
-  ///
-  /// - Parameter from: A key path to the anchor to equate to on the other view.
-  /// - Parameter to: An optional keypath to the anchor to equate `keyPath` to. If `nil` `keyPath` will be used as the
-  ///     target as well.
-  /// - Parameter otherView: The view to constrain the target to. Defaults to the view's parent.
-  ///
-  /// - Returns: A `Constraint` that creates an equality constraint.
-  public static func equal<Axis, Anchor>(
-    _ from: KeyPath<UIView, Anchor>,
-    _ to: KeyPath<UIScrollView, Anchor>,
-    otherView: UIScrollView
-  ) -> Constraint where Anchor: NSLayoutAnchor<Axis> {
-    .init { view, parent, constant, _ in
-      let anchor = view[keyPath: from]
-      let comparison = otherView[keyPath: to]
-      return anchor.constraint(equalTo: comparison, constant: constant)
-    }
+/// Generates an "equal" `Constraint` between a view and a scroll view's layout guides.
+///
+/// - Parameter from: A key path to the anchor to equate to on the other view.
+/// - Parameter to: An optional keypath to the anchor to equate `keyPath` to. If `nil` `keyPath` will be used as the
+///     target as well.
+/// - Parameter otherView: The view to constrain the target to. Defaults to the view's parent.
+///
+/// - Returns: A `Constraint` that creates an equality constraint.
+public func equal<Axis, Anchor>(
+  _ from: KeyPath<UIView, Anchor>,
+  _ to: KeyPath<UIScrollView, Anchor>,
+  otherView: UIScrollView
+) -> Constraint where Anchor: NSLayoutAnchor<Axis> {
+  .init { view, parent, constant, _ in
+    let anchor = view[keyPath: from]
+    let comparison = otherView[keyPath: to]
+    return anchor.constraint(equalTo: comparison, constant: constant)
   }
-
 }
